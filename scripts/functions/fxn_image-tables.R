7f2c9339790255d10d42f4026d59f7290ee4d242
morethangray <6456836+morethangray@users.noreply.github.com>
2024-05-03 14:00:54 -0700

    draft function to simplify comments

--- scripts/functions/fxn_image-tables.R
+++ scripts/functions/fxn_image-tables.R
@@ -3103,6 +3103,54 @@ list_hide_images_tidy_3 <-
                              "count_2")) %>%
   pull(column_order)
 
+# ---------------------------------------------------------- -----
+# _CLEAN ----
+# Simplify comments ----
+# index_path = path_table_qc
+list_id_qc <- 
+  dlog %>%
+  filter(done_qc == TRUE, 
+         done_vault == FALSE) %>%
+  pull(id)
+
+fxn_simplify_comments <- function(index_site, index_path){
+  
+  fxn_define_camera_project(index_site)
+  
+  # 1. Read all xlsx files in a folder
+  file_paths <- list.files(path = index_path, 
+                           pattern = "\\.xlsx$", 
+                           full.names = TRUE)
+  
+  # 2. Extract distinct comments from each file
+  comments_list <- map(file_paths, function(file_path) {
+    # Read the file
+    data <- read_xlsx(file_path)
+    
+    # Check if 'comments' column exists
+    if("comments" %in% names(data)) {
+      # Extract distinct comments
+      distinct(data, comments) %>% pull(comments)
+    } else {
+      # Return NA if 'comments' column does not exist
+      NA
+    }
+  })
+  
+  # Flatten the list of comments into a single vector
+  all_comments <- unlist(comments_list)
+  
+  # Remove NAs and extract unique comments across all files
+  distinct_comments <- unique(na.omit(all_comments))
+  
+  # 3. Create a tibble with all distinct comments
+  final_data <- tibble(distinct_comments = distinct_comments)
+}
+final_data %>%
+  write_csv(here(path_out, 
+                 "distinct-comments.csv"))
+ 
+# Revise id fields: image_id, image_file ----
 # ========================================================== -----
 
 
